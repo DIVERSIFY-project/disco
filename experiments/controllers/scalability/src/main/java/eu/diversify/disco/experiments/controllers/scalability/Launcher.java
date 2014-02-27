e74a370544ca29c542562a2116bbc30e9191c641
Franck Chauvel <franck.chauvel@sintef.no>
2014-02-27 09:14:29 +0100

    Removing the default setup file from the scalability experiment

--- experiments/controllers/scalability/src/main/java/eu/diversify/disco/experiments/controllers/scalability/Launcher.java
+++ experiments/controllers/scalability/src/main/java/eu/diversify/disco/experiments/controllers/scalability/Launcher.java
@@ -15,23 +15,6 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with Disco.  If not, see <http://www.gnu.org/licenses/>.
  */
-/**
- *
- * This file is part of Disco.
- *
- * Disco is free software: you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * Disco is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Disco. If not, see <http://www.gnu.org/licenses/>.
- */
 package eu.diversify.disco.experiments.controllers.scalability;
 
 import eu.diversify.disco.experiments.commons.Runner;
@@ -45,13 +28,11 @@ import eu.diversify.disco.experiments.commons.Runner;
  */
 public class Launcher {
 
-    final static String DEFAULT_CONFIG_FILE = "setup.yml";
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         Runner runner = new Runner();
-        runner.run(ScalabilitySetup.class, DEFAULT_CONFIG_FILE);
+        runner.run(ScalabilitySetup.class, args);
     }
 }
