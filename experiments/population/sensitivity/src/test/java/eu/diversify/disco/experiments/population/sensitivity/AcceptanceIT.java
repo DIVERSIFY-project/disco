d55868e59c2b6a6855b935f99c41bc816de03db3
Franck Chauvel <franck.chauvel@sintef.no>
2014-04-03 12:23:46 +0200

    Adding an acceptance test in the sensitivity experiment

--- experiments/population/sensitivity/src/test/java/eu/diversify/disco/experiments/population/sensitivity/AcceptanceIT.java
+++ experiments/population/sensitivity/src/test/java/eu/diversify/disco/experiments/population/sensitivity/AcceptanceIT.java
@@ -15,7 +15,41 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with Disco.  If not, see <http://www.gnu.org/licenses/>.
  */
-package eu.diversify.disco.experiments.controllers.singlerun;
+/**
+ *
+ * This file is part of Disco.
+ *
+ * Disco is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Disco is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Disco. If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ *
+ * This file is part of Disco.
+ *
+ * Disco is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Disco is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Disco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package eu.diversify.disco.experiments.population.sensitivity;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -35,22 +69,26 @@ import org.junit.Test;
 
 public class AcceptanceIT {
 
+    private String[] expectedOutputs = new String[]{
+        "species",
+        "individuals",
+        "distribution"};
     private String directory;
     private String standardOutput;
     private String errorOutput;
+    public String experimentName = "sensitivity";
 
-    
     @Test
     public void acceptanceTest() throws IOException, InterruptedException {
-        unzipDistribution("singlerun-dist.zip");
-        setUpDirectory("target/singlerun");
-        runCommand("java -jar singlerun-final.jar");
+        unzipDistribution(getDistributionName());
+        setUpDirectory(getWorkingDirectory());
+        runCommand(getRunCommand());
         checkLicensing();
         checkCsvOutput();
         checkPdfOutput();
         checkNoError();
     }
-    
+
     private void unzipDistribution(String archiveName) throws IOException {
         String fileName = escape(archiveName);
         ZipFile zipFile = new ZipFile(fileName);
@@ -79,41 +117,42 @@ public class AcceptanceIT {
         Process p = builder.start();
 
         BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-
-        BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-
-        // read the output from the command
         standardOutput = "";
         String line = null;
         while ((line = stdInput.readLine()) != null) {
             standardOutput += line;
         }
 
-        // read any errors from the attempted command
+        BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
         errorOutput = "";
         while ((line = stdError.readLine()) != null) {
             errorOutput = "";
         }
-        
+
         p.waitFor();
     }
 
     private void checkCsvOutput() {
-        File csvOutput = new File("target/singlerun/results.csv");
-        assertThat("CSV generated", csvOutput.exists());
+        for (String name : expectedOutputs) {
+            final String path = String.format("target/sensitivity/%s.csv", name);
+            File csvFile = new File(path);
+            assertThat("CSV generated", csvFile.exists());
+        }
     }
 
     private void checkPdfOutput() {
-        File pdfOutput = new File("target/singlerun/results.pdf");
-        assertThat("PDF generated", pdfOutput.exists());
+        for (String name : expectedOutputs) {
+            final String path = String.format("target/sensitivity/%s.pdf", name);
+            File pdfFile = new File(path);
+            assertThat("PDF generated", pdfFile.exists());
+        }
     }
 
     private void checkNoError() {
         assertThat("no error reported in stdout", standardOutput, not(containsString("Error")));
         assertThat("no error reported in stdout", errorOutput, not(containsString("Error")));
     }
-    
-    
+
     private void checkLicensing() {
         assertThat("copyright displayed", standardOutput, containsString("Copyright (C) 2013 SINTEF ICT"));
         assertThat("license displayed", standardOutput, containsString("LGPLv3+"));
@@ -121,5 +160,17 @@ public class AcceptanceIT {
 
     private String escape(String directory) {
         return directory.replaceAll("\"", "");
+    }
+
+    public String getDistributionName() {
+        return String.format("%s-dist.zip", experimentName);
+    }
+
+    public String getWorkingDirectory() {
+        return String.format("target/%s", experimentName);
+    }
+
+    public String getRunCommand() {
+        return String.format("java -jar %s-final.jar", experimentName);
     }
 }
